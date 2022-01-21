6eafda8a487e5c0bd9080fe5acf84366a0c1ecea
Evgheni Pascalov <evgheni.pascalov@duke-energy.com>
2022-01-20 23:55:21 -0500

    clean up

--- snake.js
+++ snake.js
@@ -45,7 +45,7 @@ function initSnake() {
             CURRENT_SEED = undefined;
         }
     } else {
-        initKeyListeners();
+        // initKeyListeners();
         initButtonsListeners();
     }
 
